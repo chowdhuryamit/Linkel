b20f66035fe231f6dba2af9505203473816e4ce6
amit chowdhury <camit8546@gmail.com>
2025-07-10 13:52:22 +0530

    dotenv config

--- Backend/src/firebase/firebase.config.js
+++ Backend/src/firebase/firebase.config.js
@@ -1,11 +1,24 @@
-import admin  from "firebase-admin";
-import {createRequire} from 'module'
+// import admin  from "firebase-admin";
+// import {createRequire} from 'module'
 
-const require = createRequire(import.meta.url);
-const serviceAccout =require('../firebase/serviceAccountKey.json')
+// const require = createRequire(import.meta.url);
+// const serviceAccout =require('../firebase/serviceAccountKey.json')
+
+// admin.initializeApp({
+//   credential: admin.credential.cert(serviceAccout), // or use serviceAccountKey.json
+// });
+
+// export default admin;
+
+import admin from "firebase-admin";
+import dotenv from "dotenv"
+dotenv.config();
+
+// Get the credentials from environment variable
+const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);
 
 admin.initializeApp({
-  credential: admin.credential.cert(serviceAccout), // or use serviceAccountKey.json
+  credential: admin.credential.cert(serviceAccount),
 });
 
 export default admin;
