dda67d078d247372845f38b33dcb21c1e984f548
amit chowdhury <camit8546@gmail.com>
2025-07-10 15:46:39 +0530

    frontend origin set

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -6,7 +6,7 @@ import cookieParser from 'cookie-parser'
 const app=express();
 
 app.use(cors({
-   origin:"http://localhost:5173",
+   origin:"https://linkelfrontend.onrender.com",
    credentials: true ,
    exposedHeaders: ["Set-Cookie"],
 }));
